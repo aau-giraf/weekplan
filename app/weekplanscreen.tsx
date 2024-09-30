b2d6dd1880ee737cfb3decf0357d4b4452f1e70e
MaksimHamko <112936523+MaksimHamko@users.noreply.github.com>
2024-09-30 14:25:00 +0200

    Add button and create activity form

--- app/weekplanscreen.tsx
+++ app/weekplanscreen.tsx
@@ -1,37 +1,46 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import {View, StyleSheet} from "react-native";
 import WeekdayHeader from "../components/WeekdayHeader";
 import TaskItem from "../components/TaskItem";
+import AddButton from "../components/AddButton";
 
 const Weekplanscreen = () => {
+    const [selectedDay, setSelectedDay] = useState<string | null>(null);
+    const [selectedDate, setSelectedDate] = useState<string | null>(null);
   const handleDayPress = (day: string, date: Date) => {
     const formattedDate = date.toLocaleDateString("da-DK", {
       day: "numeric",
       month: "short",
       year: "numeric",
     });
+      setSelectedDay(day);
+      setSelectedDate(formattedDate);
     console.log(`Selected day: ${day}, Date: ${formattedDate}`);
   };
 
-  return (
-    <View style={styles.container}>
-      <WeekdayHeader onDayPress={handleDayPress} />
-      <TaskItem time={"08:00-09:00"} label={"Helløj"} />
-      <TaskItem time={"10:00-11:00"} label={"Hello"} />
-      <TaskItem time={"12:00-14:00"} label={"Heeey"} />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <WeekdayHeader onDayPress={handleDayPress}/>
+
+            {selectedDay && selectedDate && (
+                <AddButton label='+' pathname={"./additem"} params={{day: selectedDay, date: selectedDate}}/>
+            )}
+        </View>
+
+    );
 };
+
+
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  contentText: {
-    color: "#ECEFF1",
-    marginTop: 20,
-  },
+    container: {
+        padding: 10,
+        flex: 1,
+        paddingHorizontal: 20,
+    },
+    contentText: {
+        color: "#ECEFF1",
+        marginTop: 20,
+    },
 });
 
 export default Weekplanscreen;
