772809eaf1fc017a5929a30c533db1746aeb6f04
Stoldt <lstold22@student.aau.dk>
2024-09-30 14:00:30 +0200

    Feature added: Swipeable taskitem. Still needs logic implemented. Had to remove elements from weekplanscreen as the props are currently not implemnted

--- app/weekplanscreen.tsx
+++ app/weekplanscreen.tsx
@@ -1,46 +1,33 @@
-import React, {useState} from "react";
-import {View, StyleSheet} from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import WeekdayHeader from "../components/WeekdayHeader";
-import TaskItem from "../components/TaskItem";
-import AddButton from "../components/AddButton";
 
-const Weekplanscreen = () => {
-    const [selectedDay, setSelectedDay] = useState<string | null>(null);
-    const [selectedDate, setSelectedDate] = useState<string | null>(null);
+const WeekPlanScreen = () => {
   const handleDayPress = (day: string, date: Date) => {
     const formattedDate = date.toLocaleDateString("da-DK", {
       day: "numeric",
       month: "short",
       year: "numeric",
     });
-      setSelectedDay(day);
-      setSelectedDate(formattedDate);
     console.log(`Selected day: ${day}, Date: ${formattedDate}`);
   };
 
-    return (
-        <View style={styles.container}>
-            <WeekdayHeader onDayPress={handleDayPress}/>
-
-            {selectedDay && selectedDate && (
-                <AddButton label='+' pathname={"./additem"} params={{day: selectedDay, date: selectedDate}}/>
-            )}
-        </View>
-
-    );
+  return (
+    <View style={styles.container}>
+      <WeekdayHeader onDayPress={handleDayPress} />
+    </View>
+  );
 };
-
-
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        flex: 1,
-        paddingHorizontal: 20,
-    },
-    contentText: {
-        color: "#ECEFF1",
-        marginTop: 20,
-    },
+  container: {
+    padding: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  contentText: {
+    color: "#ECEFF1",
+    marginTop: 20,
+  },
 });
 
-export default Weekplanscreen;
+export default WeekPlanScreen;
