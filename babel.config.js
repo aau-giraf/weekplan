4d0ca7185fbce756a505b5fef1d55567d027458d
Stoldt <lstold22@student.aau.dk>
2024-09-30 14:00:30 +0200

    Feature added: Swipeable taskitem. Still needs logic implemented. Had to remove elements from weekplanscreen as the props are currently not implemnted

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+        //THIS PACKAGE MUST BE LAST FOR SOME REASON
+      'react-native-reanimated/plugin',
+    ],
   };
 };
