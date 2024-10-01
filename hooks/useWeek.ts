778597e68442dcc550ecbc16a91c9e9c68c5a53c
simo205j <113094975+Simo205j@users.noreply.github.com>
2024-10-01 14:06:26 +0200

    Week Selection
    Feature such that you can select different weeks from 1 year previous, current year and next year

--- hooks/useWeek.ts
+++ hooks/useWeek.ts
@@ -17,17 +17,32 @@ const useWeek = (initialDate = new Date()) => {
     setCurrentWeek(() => nextWeek);
   };
 
+  const setWeekAndYear = (weekNumber: number, year: number) => {
+    //Months are indexed from 0
+    const firstDayOfYear = new Date(Date.UTC(year, 0, 1));
+    const dayOfWeek = firstDayOfYear.getUTCDay();
+    const daysOffset = dayOfWeek <= 4 ? dayOfWeek - 1 : dayOfWeek - 8;
+
+    const firstMondayOfYear = new Date(Date.UTC(year, 0, 1));
+    firstMondayOfYear.setUTCDate(firstMondayOfYear.getUTCDate() - daysOffset);
+
+    // Calculate the target date by adding weeks to the first Monday
+    firstMondayOfYear.setUTCDate(firstMondayOfYear.getUTCDate() + (weekNumber - 1) * 7);
+
+    setCurrentWeek(firstMondayOfYear);
+  };
+
+
+
   const weekNumber = getWeekNumber(currentWeek);
-  const weekDates = useMemo(
-    () => getWeekDates(currentWeek),
-    [currentWeek]
-  );
+  const weekDates = useMemo(() => getWeekDates(currentWeek), [currentWeek]);
 
   return {
     weekDates,
     goToPreviousWeek,
     goToNextWeek,
     weekNumber,
+    setWeekAndYear,
   };
 };
 
