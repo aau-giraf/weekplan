af703a81ebc13d6c49b93fc18e048ad4b7334856
DanielRBech <dbech22@student.aau.dk>
2024-10-01 14:49:28 +0200

    Forgot to add functionality to NormalCalender

--- components/NormalCalender.tsx
+++ components/NormalCalender.tsx
@@ -4,14 +4,16 @@ import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 type CalenderProps = {
     minDate: Date,
+    onTimeChange: (date: Date) => void;
 }
 
-const NormalCalender: React.FC<CalenderProps> = ({ minDate }) => {
+const NormalCalender: React.FC<CalenderProps> = ({ minDate, onTimeChange }) => {
     const [date, setDate] = useState(new Date());
 
     const handleDateChange = (event: any, selectedDate: Date | undefined) => {
         if (selectedDate) {
             setDate(selectedDate);
+            onTimeChange(selectedDate);
         }
     };
 
