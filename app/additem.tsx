36fb173ed6f4b0b284867647defa2383801ffc9e
MertzAndreas <andreasmertz11@gmail.com>
2024-10-08 10:50:40 +0200

    Updateed RNDateTimePicker to DateTimePicker

--- app/additem.tsx
+++ app/additem.tsx
@@ -1,5 +1,5 @@
-import { useRouter } from "expo-router";
-import React, { useState } from "react";
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -11,10 +11,10 @@ import {
   ScrollView,
   View,
   TouchableOpacity,
-} from "react-native";
-import RNDateTimePicker from "@react-native-community/datetimepicker";
-import { formattedDate } from "../utils/formattedDate";
-import { useDate } from "../providers/DateProvider";
+} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { formattedDate } from '../utils/formattedDate';
+import { useDate } from '../providers/DateProvider';
 
 interface FormData {
   label: string;
@@ -27,8 +27,8 @@ const AddItem = () => {
   const router = useRouter();
   const { selectedDate } = useDate();
   const [formData, setFormData] = useState<FormData>({
-    label: "",
-    description: "",
+    label: '',
+    description: '',
     startTime: new Date(0, 0, 0, 0, 0),
     endTime: new Date(0, 0, 0, 23, 59),
   });
@@ -43,13 +43,13 @@ const AddItem = () => {
   const handleSubmit = () => {
     const { label, description, startTime, endTime } = formData;
 
-    const formattedStartTime = startTime.toLocaleTimeString("da-DK", {
-      hour: "2-digit",
-      minute: "2-digit",
+    const formattedStartTime = startTime.toLocaleTimeString('da-DK', {
+      hour: '2-digit',
+      minute: '2-digit',
     });
-    const formattedEndTime = endTime.toLocaleTimeString("da-DK", {
-      hour: "2-digit",
-      minute: "2-digit",
+    const formattedEndTime = endTime.toLocaleTimeString('da-DK', {
+      hour: '2-digit',
+      minute: '2-digit',
     });
 
     console.log(
@@ -62,9 +62,8 @@ const AddItem = () => {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <KeyboardAvoidingView
         style={styles.container}
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-        keyboardVerticalOffset={80}
-      >
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={80}>
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
           <Text style={styles.headerText}>
             Opret en begivenhed til {formattedDate(selectedDate)}
@@ -74,7 +73,7 @@ const AddItem = () => {
             style={styles.input}
             placeholder="Navn"
             value={formData.label}
-            onChangeText={(text) => handleInputChange("label", text)}
+            onChangeText={(text) => handleInputChange('label', text)}
             returnKeyType="done"
           />
 
@@ -82,14 +81,14 @@ const AddItem = () => {
             style={styles.description}
             placeholder="Beskrivelse"
             value={formData.description}
-            onChangeText={(text) => handleInputChange("description", text)}
+            onChangeText={(text) => handleInputChange('description', text)}
             multiline
             returnKeyType="done"
           />
 
           <View style={styles.pickerContainer}>
             <Text style={styles.header}>Vælg start tid</Text>
-            <RNDateTimePicker
+            <DateTimePicker
               mode="time"
               value={formData.startTime}
               maximumDate={formData.endTime}
@@ -97,7 +96,7 @@ const AddItem = () => {
               display="default"
               onChange={(_event, selectedDate) => {
                 if (!selectedDate) return;
-                handleInputChange("startTime", selectedDate);
+                handleInputChange('startTime', selectedDate);
               }}
               style={styles.timePicker}
             />
@@ -105,7 +104,7 @@ const AddItem = () => {
 
           <View style={styles.pickerContainer}>
             <Text style={styles.header}>Vælg slut tid</Text>
-            <RNDateTimePicker
+            <DateTimePicker
               mode="time"
               value={formData.endTime}
               minimumDate={formData.startTime}
@@ -113,7 +112,7 @@ const AddItem = () => {
               display="default"
               onChange={(_event, selectedDate) => {
                 if (!selectedDate) return;
-                handleInputChange("endTime", selectedDate);
+                handleInputChange('endTime', selectedDate);
               }}
               style={styles.timePicker}
             />
@@ -121,8 +120,7 @@ const AddItem = () => {
 
           <TouchableOpacity
             style={[styles.button, styles.addButton]}
-            onPress={handleSubmit}
-          >
+            onPress={handleSubmit}>
             <Text style={styles.buttonText}>Tilføj</Text>
           </TouchableOpacity>
         </ScrollView>
@@ -135,63 +133,63 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     flex: 1,
-    backgroundColor: "#f9f9f9",
+    backgroundColor: '#f9f9f9',
   },
   headerText: {
     fontSize: 20,
-    fontWeight: "600",
+    fontWeight: '600',
     marginBottom: 20,
-    textAlign: "center",
-    color: "#333",
+    textAlign: 'center',
+    color: '#333',
   },
   input: {
     height: 48,
-    borderColor: "#ccc",
+    borderColor: '#ccc',
     borderWidth: 1,
     marginBottom: 20,
     paddingHorizontal: 15,
     borderRadius: 8,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   description: {
     height: 80,
-    borderColor: "#ccc",
+    borderColor: '#ccc',
     borderWidth: 1,
     marginBottom: 20,
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 8,
-    backgroundColor: "#fff",
-    textAlignVertical: "top",
+    backgroundColor: '#fff',
+    textAlignVertical: 'top',
   },
   button: {
-    backgroundColor: "#5A67D8",
+    backgroundColor: '#5A67D8',
     paddingVertical: 12,
     paddingHorizontal: 20,
     borderRadius: 8,
     marginVertical: 10,
-    alignItems: "center",
+    alignItems: 'center',
   },
   addButton: {
-    backgroundColor: "#38A169",
+    backgroundColor: '#38A169',
   },
   buttonText: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: '500',
   },
   pickerContainer: {
     marginBottom: 20,
-    alignItems: "center",
+    alignItems: 'center',
   },
   header: {
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: '500',
     marginBottom: 10,
-    color: "#333",
+    color: '#333',
   },
   timePicker: {
-    position: "static",
+    position: 'static',
     marginRight: 5,
   },
 });
