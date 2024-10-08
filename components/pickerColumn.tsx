39b1afc5a1ed38e8aab55f25b282bf80d87cab11
MertzAndreas <andreasmertz11@gmail.com>
2024-10-08 11:28:00 +0200

    Changed WeekSelection picker to be useable on phone

--- components/pickerColumn.tsx
+++ components/pickerColumn.tsx
@@ -3,51 +3,48 @@ import { View, StyleSheet, Text } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 interface PickerColumnProps {
-    title: string;
-    weeksOrYear: number[];
-    selectedValue: number;
-    setSelectedValue: React.Dispatch<React.SetStateAction<number>>;
+  title: string;
+  weeksOrYear: number[];
+  selectedValue: number;
+  setSelectedValue: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const PickerColumn: React.FC<PickerColumnProps> = ({
-                                                       title,
-                                                       weeksOrYear,
-                                                       selectedValue,
-                                                       setSelectedValue
-                                                   }) => (
-    <View style={styles.pickerColumn}>
-        <Text style={styles.pickerTitle}>{title}</Text>
-        <Picker
-            selectedValue={selectedValue}
-            onValueChange={(itemValue) => setSelectedValue(itemValue)}
-            style={styles.picker}
-            itemStyle={styles.pickerItem}
-        >
-            {weeksOrYear.map((item) => (
-                <Picker.Item key={item} label={item.toString()} value={item} />
-            ))}
-        </Picker>
-    </View>
+  title,
+  weeksOrYear,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={styles.pickerColumn}>
+    <Text style={styles.pickerTitle}>{title}</Text>
+    <Picker
+      selectedValue={selectedValue}
+      onValueChange={(itemValue) => setSelectedValue(itemValue)}
+      style={styles.picker}
+      itemStyle={styles.pickerItem}>
+      {weeksOrYear.map((item) => (
+        <Picker.Item key={item} label={item.toString()} value={item} />
+      ))}
+    </Picker>
+  </View>
 );
 
 const styles = StyleSheet.create({
-    pickerColumn: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    pickerTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 10,
-    },
-    picker: {
-        height: 150,
-        width: 150,
-    },
-    pickerItem: {
-        fontSize: 18,
-    },
+  pickerColumn: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  pickerTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  picker: {
+    width: 150,
+  },
+  pickerItem: {
+    fontSize: 18,
+  },
 });
 
 export default PickerColumn;
