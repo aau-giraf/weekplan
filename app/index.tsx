b13c51876e4ee13edeb389ec9ba09630422395ef
Azzi <arminalicajic1324@hotmail.com>
2024-10-08 11:54:16 +0200

    browser fix

--- app/index.tsx
+++ app/index.tsx
@@ -2,47 +2,47 @@ import { Link } from 'expo-router';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const HomePage = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Home Page</Text>
-            <Link href="/example" style={styles.link}>
-                <Text style={styles.linkText}>Go to Example Page</Text>
-            </Link>
-            <Link href="/login" style={styles.link}>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Go to Login Page</Text>
-                </TouchableOpacity>
-            </Link>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Page</Text>
+      <Link href="/example" style={styles.link}>
+        <Text style={styles.link}>Go to Example Page</Text>
+      </Link>
+      <Link href="/login" style={styles.link}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Go to Login Page</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
 };
 
 // Styles for the HomePage component
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#A7C6ED',
-    },
-    title: {
-        fontSize: 24,
-        marginBottom: 20,
-        fontWeight: 'bold',
-    },
-    link: {
-        marginVertical: 10,
-    },
-    button: {
-        backgroundColor: '#B7E2D8',
-        padding: 15,
-        borderRadius: 5,
-    },
-    buttonText: {
-        color: '#4A4A4A',
-        textAlign: 'center',
-        fontSize: 18,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#A7C6ED',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  link: {
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: '#B7E2D8',
+    padding: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#4A4A4A',
+    textAlign: 'center',
+    fontSize: 18,
+  },
 });
 
 export default HomePage;
