b5cc6e359dceea223fdea651fc1b0b947f6e9e08
Azzi <arminalicajic1324@hotmail.com>
2024-10-08 11:10:21 +0200

    .

--- app/mainScreen.tsx
+++ app/mainScreen.tsx
@@ -1,51 +1,52 @@
-import React from 'react'; 
+import React from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const MainScreen = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate('Weekplanner')}
-                activeOpacity={0.8}
-            >
-                <Text style={styles.buttonText}>Weekplanner</Text>
-            </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <Text>
+        <Text style={styles.buttonText}>Weekplanner</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Weekplanner')}
+          activeOpacity={0.8}
+        ></TouchableOpacity>
+      </Text>
 
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate('Foodplanner')}
-                activeOpacity={0.8}
-            >
-                <Text style={styles.buttonText}>Foodplanner</Text>
-            </TouchableOpacity>
-
-        </View>
-    );
+      <Text>
+        <Text style={styles.buttonText}>Foodplanner</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Foodplanner')}
+          activeOpacity={0.8}
+        ></TouchableOpacity>
+      </Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#A7C6ED',
-    },
-    button: {
-        backgroundColor: '#B7E2D8',
-        padding: 15,
-        borderRadius: 5,
-        marginVertical: 10,
-        width: '80%',
-    },
-    buttonText: {
-        color: '#4A4A4A',
-        textAlign: 'center',
-        fontSize: 22,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#A7C6ED',
+  },
+  button: {
+    backgroundColor: '#B7E2D8',
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: '80%',
+  },
+  buttonText: {
+    color: '#4A4A4A',
+    textAlign: 'center',
+    fontSize: 22,
+  },
 });
 
 export default MainScreen;
