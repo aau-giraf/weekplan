612e6c5fb2c11ef9a5f9df0977b555e586f76ed1
MertzAndreas <andreasmertz11@gmail.com>
2024-10-21 15:25:27 +0200

    Testing refine

--- __tests__/useValidation.test.tsx
+++ __tests__/useValidation.test.tsx
@@ -13,7 +13,7 @@ describe("useValidation hook", () => {
   test("returns valid if the form data is valid", () => {
     const mockData: FormData = { title: "Title", description: "Beskrivelse" };
     const { result } = renderHook(() =>
-      useValidation({ formData: mockData, schema }),
+      useValidation({ formData: mockData, schema })
     );
     expect(result.current.valid).toBe(true);
   });
@@ -21,7 +21,7 @@ describe("useValidation hook", () => {
   test("returns errors if the form data is invalid", () => {
     const mockData: FormData = { title: "", description: "" };
     const { result } = renderHook(() =>
-      useValidation({ formData: mockData, schema }),
+      useValidation({ formData: mockData, schema })
     );
     expect(result.current.valid).toBe(false);
     expect(result.current.errors?.title?._errors).toEqual([
@@ -34,7 +34,7 @@ describe("useValidation hook", () => {
   test("returns errors for invalid fields when only one is invalid", () => {
     const mockData: FormData = { title: "Title", description: "" };
     const { result } = renderHook(() =>
-      useValidation({ formData: mockData, schema }),
+      useValidation({ formData: mockData, schema })
     );
     expect(result.current.valid).toBe(false);
     expect(result.current.errors?.title?._errors).toBeUndefined();
@@ -48,7 +48,7 @@ describe("useValidation hook", () => {
       description: null as unknown as string,
     };
     const { result } = renderHook(() =>
-      useValidation({ formData: mockData, schema }),
+      useValidation({ formData: mockData, schema })
     );
     expect(result.current.valid).toBe(false);
     expect(result.current.errors?.title?._errors).toEqual([
@@ -58,10 +58,42 @@ describe("useValidation hook", () => {
       "Expected string, received null",
     ]);
   });
+
+  test("Testing refine function returns expected output false", () => {
+    const customValid = (val: number) => (val > 5 ? true : false);
+    const schema2 = z.object({
+      value: z
+        .number()
+        .refine(customValid, { message: "Value must be greater than 5" }),
+    });
+    type FormData2 = z.infer<typeof schema2>;
+    const mockData: FormData2 = { value: 4 };
+    const { result } = renderHook(() =>
+      useValidation({ formData: mockData, schema: schema2 })
+    );
+
+    expect(result.current.valid).toBe(false);
+  });
+
+  test("Testing refine function returns expected output true", () => {
+    const customValid = (val: number) => (val > 5 ? true : false);
+    const schema2 = z.object({
+      value: z
+        .number()
+        .refine(customValid, { message: "Value must be greater than 5" }),
+    });
+    type FormData2 = z.infer<typeof schema2>;
+    const mockData: FormData2 = { value: 6 };
+    const { result } = renderHook(() =>
+      useValidation({ formData: mockData, schema: schema2 })
+    );
+
+    expect(result.current.valid).toBe(true);
+  });
   test("returns errors for whitespace-only input", () => {
     const mockData: FormData = { title: "   ", description: "   " };
     const { result } = renderHook(() =>
-      useValidation({ formData: mockData, schema }),
+      useValidation({ formData: mockData, schema })
     );
     expect(result.current.valid).toBe(false);
     expect(result.current.errors?.title?._errors).toEqual([
