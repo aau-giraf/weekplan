adc6238681b772f5f04c28dfc29c39bdb946afbd
MertzAndreas <andreasmertz11@gmail.com>
2024-10-22 12:49:20 +0200

    Implemented Toast

---
+++ components/Toast.tsx
@@ -0,0 +1,85 @@
+import { StyleSheet, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Animated, {
+  FadeInUp,
+  FadeOutRight,
+  LinearTransition,
+} from "react-native-reanimated";
+import { colors } from "../utils/colors";
+
+type ToastNotification = "success" | "error" | "warning";
+type ToastStyle = {
+  color: string;
+  icon: keyof typeof Ionicons.glyphMap;
+};
+
+const toastMap: Record<ToastNotification, ToastStyle> = {
+  success: {
+    icon: "checkmark-circle",
+    color: colors.green,
+  },
+  error: {
+    icon: "alert-circle",
+    color: colors.crimson,
+  },
+  warning: {
+    icon: "alert-circle",
+    color: colors.orange,
+  },
+};
+
+export type ToastProps = {
+  id: number;
+  message: string;
+  type: ToastNotification;
+  onClose: (id: number) => void;
+};
+
+const Toast = ({ message, type, id, onClose }: ToastProps) => {
+  const { icon, color } = toastMap[type];
+
+  return (
+    <Animated.View
+      key={id}
+      layout={LinearTransition}
+      entering={FadeInUp}
+      exiting={FadeOutRight}
+      style={[styles.toastContainer, { backgroundColor: color }]}>
+      <Ionicons size={35} name={icon} style={styles.icon} />
+      <Text style={styles.toastMessage}>{message}</Text>
+      <Ionicons
+        size={35}
+        name={"close-outline"}
+        style={styles.icon}
+        onPress={() => onClose(id)}
+      />
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  toastContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 15,
+    backgroundColor: "white",
+    borderRadius: 5,
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+  },
+  toastMessage: {
+    color: "white",
+    fontSize: 18,
+    flex: 1,
+    textAlign: "center",
+    textShadowColor: "black",
+
+    marginHorizontal: 20,
+  },
+  icon: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
+export default Toast;
---
+++ providers/ToastProvider.tsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState, useCallback } from "react";
+import { StyleSheet } from "react-native";
+import Toast, { ToastProps } from "../components/Toast";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type ToastProviderValues = {
+  toasts: Omit<ToastProps, "onClose">[];
+  addToast: (toast: Omit<ToastProps, "id" | "onClose">) => void;
+  removeToast: (id: number) => void;
+};
+
+const ToastContext = createContext<ToastProviderValues | undefined>(undefined);
+
+const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+  const [toasts, setToasts] = useState<Omit<ToastProps, "onClose">[]>([]);
+
+  const removeToast = useCallback((id: number) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  }, []);
+
+  const addToast = useCallback(
+    (toast: Omit<ToastProps, "id" | "onClose">, duration = 100000) => {
+      const id = new Date().getTime();
+      const toastWithId = { id, duration, ...toast };
+      setToasts((prev) => [...prev, toastWithId]);
+      setTimeout(() => {
+        removeToast(id);
+      }, duration);
+    },
+    [removeToast]
+  );
+
+  return (
+    <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
+      {children}
+      <SafeAreaView style={styles.container}>
+        {toasts.map((toast) => (
+          <Toast
+            key={toast.id}
+            id={toast.id}
+            message={toast.message}
+            type={toast.type}
+            onClose={() => removeToast(toast.id)}
+          />
+        ))}
+      </SafeAreaView>
+    </ToastContext.Provider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "90%",
+    position: "absolute",
+    top: 0,
+    alignSelf: "center",
+    gap: 10,
+  },
+});
+
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  if (!context) {
+    throw new Error("useToast must be used within a ToastProvider");
+  }
+  return context;
+};
+
+export default ToastProvider;
