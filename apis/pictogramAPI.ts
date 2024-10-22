5ba8fcf4593358f5c5c7be505b23ba5f04029002
Stoldt <lstold22@student.aau.dk>
2024-10-22 14:51:24 +0200

    Revert "Fixed?"
    This reverts commit 636a6c16820ea5d64800ce79ce8eb9049ef9b098.

--- apis/pictogramAPI.ts
+++ apis/pictogramAPI.ts
@@ -1,5 +1,3 @@
-import { Platform } from "react-native";
-
 /**
  * Function for sending a request to the Arasaac API to retrieve a Pictogram
  * @param id {number} - The ID of the pictogram to be fetched.
@@ -20,20 +18,7 @@ export const fetchPictograms = async (id: number) => {
     contentType?.includes("image/jpeg")
   ) {
     const imageBlob = await res.blob();
-
-    if (Platform.OS === "ios") {
-      return URL.createObjectURL(imageBlob);
-    } else {
-      const reader = new FileReader();
-      return await new Promise((resolve, reject) => {
-        reader.onloadend = () => {
-          const base64data = reader.result;
-          resolve(base64data); // Return the Base64 string
-        };
-        reader.onerror = reject;
-        reader.readAsDataURL(imageBlob); // Read blob as Base64 string
-      });
-    }
+    return URL.createObjectURL(imageBlob);
   } else {
     throw new Error("Response er ikke et billede");
   }
