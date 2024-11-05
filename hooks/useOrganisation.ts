6e3e019e41aade9041b52428e3f160e84833cbd7
MertzAndreas <andreasmertz11@gmail.com>
2024-11-05 09:34:19 +0100

    Profile page logic (#145)
    * UI for profile
    
    * Fix to IconButton
    
    * Unusual bug
    
    * Added UI changes such as icon button animations, also partially implemented hook and API interaction for organisation interactions. Create Org works but rest is implemented in a different page
    
    * Test and requested changes
    
    * Fixed long test suit and some imports
    
    ---------
    
    Co-authored-by: Tbjerregaard <113022626+Tbjerregaard@users.noreply.github.com>

---
+++ hooks/useOrganisation.ts
@@ -0,0 +1,82 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuthentication } from "../providers/AuthenticationProvider";
+import {
+  createOrganisationsRequest,
+  deleteOrganisationRequest,
+  fetchAllOrganisationsRequest,
+} from "../apis/organisationAPI";
+
+export type OrgDTO = {
+  name: string;
+  id: number;
+};
+
+const useOrganisation = () => {
+  const { userId } = useAuthentication();
+  const queryClient = useQueryClient();
+  const queryKey = [userId!, "Organisation"];
+
+  const fetchOrgOverview = useQuery<OrgDTO[]>({
+    queryFn: async () => fetchAllOrganisationsRequest(userId!),
+    queryKey,
+    enabled: !!userId,
+  });
+
+  const deleteOrganisation = useMutation({
+    mutationFn: (orgId: number) => deleteOrganisationRequest(orgId),
+    onMutate: async (orgId) => {
+      await queryClient.cancelQueries({ queryKey });
+      const previousOrgs = queryClient.getQueryData<OrgDTO[]>(queryKey);
+      queryClient.setQueryData<OrgDTO[]>(queryKey, (oldData) => {
+        return oldData?.filter((org) => org.id !== orgId);
+      });
+
+      return previousOrgs;
+    },
+    onError: (_error, _deleted, context) => {
+      if (context) {
+        queryClient.setQueryData(queryKey, context);
+      }
+    },
+  });
+
+  const createOrganisation = useMutation({
+    mutationFn: (orgName: string) =>
+      createOrganisationsRequest(userId!, orgName),
+    onMutate: async (newOrgName) => {
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousOrgs = queryClient.getQueryData<OrgDTO[]>(queryKey);
+      queryClient.setQueryData<OrgDTO[]>(queryKey, (oldData) => [
+        ...(oldData || []),
+        { name: newOrgName, id: -1 },
+      ]);
+
+      return previousOrgs;
+    },
+    onError: (_error, _newOrg, context) => {
+      if (context) {
+        queryClient.setQueryData(queryKey, context);
+      }
+    },
+    onSuccess: (data, _variables, _context) => {
+      queryClient.setQueryData<OrgDTO[]>(queryKey, (oldData) =>
+        (oldData || []).map((org) =>
+          org.id === -1 ? { ...org, id: data.id } : org
+        )
+      );
+    },
+  });
+
+  return {
+    data: fetchOrgOverview.data,
+    isLoading: fetchOrgOverview.isLoading,
+    isError: fetchOrgOverview.isError,
+    isSuccess: fetchOrgOverview.isSuccess,
+    refetch: fetchOrgOverview.refetch,
+    createOrganisation,
+    deleteOrganisation,
+  };
+};
+
+export default useOrganisation;
--- providers/AuthenticationProvider.tsx
+++ providers/AuthenticationProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useCallback } from "react";
+import { createContext, useContext, useState, useCallback } from "react";
 import { createUserRequest } from "../apis/registerAPI";
 import { tryLogin } from "../apis/loginAPI";
 import { useToast } from "./ToastProvider";
