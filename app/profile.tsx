a9de34b33620325bd18475e62515f297fd875aae
MertzAndreas <andreasmertz11@gmail.com>
2024-11-06 15:03:05 +0100

    Authentication improvements and settings page (#174)
    Added shadow to bottomsheet and clicking besides hides it again. Implemented a 'Remember me' option for login, Added settings page, where you can configure 'remember me' and implemented the logout feature. Added mock for async-store in test according to docs

--- app/profile.tsx
+++ app/profile.tsx
@@ -80,6 +80,7 @@ const ProfilePage: React.FC = () => {
     <View style={styles.container}>
       <Animated.FlatList
         refreshing={orgIsLoading}
+        onTouchStart={() => bottomSheetRef.current?.close()}
         itemLayoutAnimation={LinearTransition}
         onRefresh={async () => await refetch()}
         data={orgData}
@@ -101,8 +102,10 @@ const ProfilePage: React.FC = () => {
                   {`${data.firstName} ${data.lastName}`}
                 </Text>
               </View>
-              <IconButton style={styles.iconMail}>
-                <Ionicons name="mail-outline" size={ScaleSize(40)} />
+              <IconButton
+                style={styles.settings}
+                onPress={() => router.push("/settings")}>
+                <Ionicons name="settings-outline" size={ScaleSize(40)} />
               </IconButton>
             </View>
             <View style={styles.organizationsContainer}>
@@ -160,7 +163,8 @@ const AddBottomSheet = ({
       enablePanDownToClose={true}
       keyboardBlurBehavior="restore"
       index={-1}
-      onClose={() => setName("")}>
+      onClose={() => setName("")}
+      style={{ shadowRadius: 20, shadowOpacity: 0.3 }}>
       <BottomSheetScrollView contentContainerStyle={styles.sheetContent}>
         <Text style={SharedStyles.header}>Organisation navn</Text>
         <BottomSheetTextInput
@@ -235,7 +239,7 @@ const styles = StyleSheet.create({
     fontSize: ScaleSize(24),
     marginRight: 10,
   },
-  iconMail: {
+  settings: {
     top: ScaleSize(10),
     right: ScaleSize(30),
   },
