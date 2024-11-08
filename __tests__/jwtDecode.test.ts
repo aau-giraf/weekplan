d3a62f6beed979ffc308a234dbd4fec2333653b0
Tbjerregaard <niflheim1997@gmail.com>
2024-11-08 21:49:13 +0100

    this works 200%

--- __tests__/jwtDecode.test.ts
+++ __tests__/jwtDecode.test.ts
@@ -6,8 +6,24 @@ describe("isTokenExpired", () => {
     expect(isTokenExpired(expiredToken)).toBe(true);
   });
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   it("returns false for valid token", () => {
     const validToken = "header." + btoa(JSON.stringify({ exp: Date.now() / 1000 + 1000 })) + ".signature";
     expect(isTokenExpired(validToken)).toBe(false);
   });
 });
+
