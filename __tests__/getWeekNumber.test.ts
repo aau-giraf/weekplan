65c1511eac72ea3c2d245b2c35d275935a267f2b
Tbjerregaard <Tbjerregaard@users.noreply.github.com>
2024-11-08 17:58:28 +0000

    Prettified Code!

--- __tests__/getWeekNumber.test.ts
+++ __tests__/getWeekNumber.test.ts
@@ -7,35 +7,6 @@ describe("getWeekNumber", () => {
     expect(weekNumber).toBe(24);
   });
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   test("should return correct week number for a date in the beginning of the year", () => {
     const date = new Date("2023-01-05");
     const weekNumber = getWeekNumber(date);
