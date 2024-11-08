0a61ff9fd1aea58f85068c5a6bdef397de1dbb0f
Tbjerregaard <niflheim1997@gmail.com>
2024-11-08 17:05:41 +0100

    testing prettier this definitely works

--- __tests__/ActivityTimePicker.test.tsx
+++ __tests__/ActivityTimePicker.test.tsx
@@ -4,7 +4,7 @@ import { Platform } from "react-native";
 import TimePicker from "../components/TimePicker";
 import formatTimeHHMM from "../utils/formatTimeHHMM";
 
-describe("ActivityTimePicker", () => {
+describe('ActivityTimePicker', () => {
   const mockOnChange = jest.fn();
 
   const initialProps = {
