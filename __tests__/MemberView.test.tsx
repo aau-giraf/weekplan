0527417e158e5c2192fcc7e0cf1eb10446564f3f
Tbjerregaard <niflheim1997@gmail.com>
2024-11-08 19:08:04 +0100

    this works now 5%3232

--- __tests__/MemberView.test.tsx
+++ __tests__/MemberView.test.tsx
@@ -21,6 +21,16 @@ describe("MemberView Component", () => {
   });
 
   it("renders only MAX_DISPLAYED_MEMBERS when members exceed limit", () => {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     const members = Array.from({ length: 10 }, (_, i) =>
       generateMember(i.toString(), i.toString() + "@mail")
     );
