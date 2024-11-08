6f474bae6cd4f3c27e826f9e798a2e5b9ae6570c
Tbjerregaard <niflheim1997@gmail.com>
2024-11-08 16:51:13 +0100

    testing prettier

--- __tests__/ActivityTimePicker.test.tsx
+++ __tests__/ActivityTimePicker.test.tsx
@@ -4,7 +4,7 @@ import { Platform } from "react-native";
 import TimePicker from "../components/TimePicker";
 import formatTimeHHMM from "../utils/formatTimeHHMM";
 
-describe('testforprettier', () => {
+describe("ActivityTimePicker", () => {
   const mockOnChange = jest.fn();
 
   const initialProps = {
