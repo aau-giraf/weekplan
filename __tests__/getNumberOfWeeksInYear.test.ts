3af23c2957c9b79b0f203fc9d9e186ea05c7946a
Tbjerregaard <Tbjerregaard@users.noreply.github.com>
2024-11-08 20:54:45 +0000

    Prettified Code!

--- __tests__/getNumberOfWeeksInYear.test.ts
+++ __tests__/getNumberOfWeeksInYear.test.ts
@@ -10,10 +10,7 @@ describe("getNumberOfWeeksInYear", () => {
 describe("getNumberOfWeeksInYear", () => {
   test("should return 53 weeks for a leap year", () => {
     const year = 2026;
-    
-    
-    
-    
+
     const weeks = getNumberOfWeeksInYear(year);
     expect(weeks).toBe(53);
   });
