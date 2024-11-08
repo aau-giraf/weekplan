7fca5555434b6fbdc7ab9b270aa2e08f6f763c1e
Tbjerregaard <Tbjerregaard@users.noreply.github.com>
2024-11-08 16:14:16 +0000

    Prettified Code!

--- __tests__/ActivityTimePicker.test.tsx
+++ __tests__/ActivityTimePicker.test.tsx
@@ -7,23 +7,8 @@ import formatTimeHHMM from "../utils/formatTimeHHMM";
 describe("ActivityTimePicker", () => {
   const mockOnChange = jest.fn();
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   const initialProps = {
-    title: 'Select Time',
+    title: "Select Time",
     value: new Date(new Date().setHours(14, 30)),
     onChange: mockOnChange,
   };
