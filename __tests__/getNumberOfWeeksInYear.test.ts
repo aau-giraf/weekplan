a46e8dcef2754d0f1f40306e70936aa12b645ba6
Tbjerregaard <niflheim1997@gmail.com>
2024-11-08 22:22:39 +0100

    this works 800%

--- __tests__/getNumberOfWeeksInYear.test.ts
+++ __tests__/getNumberOfWeeksInYear.test.ts
@@ -8,6 +8,11 @@ describe("getNumberOfWeeksInYear", () => {
   });
 });
 describe("getNumberOfWeeksInYear", () => {
+  
+  
+  
+  
+  
   test("should return 53 weeks for a leap year", () => {
     const year = 2026;
 
