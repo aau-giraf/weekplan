4896073eea107b55f6f769bf5d744b7b90b0af28
Azzi <arminalicajic1324@hotmail.com>
2024-11-09 01:24:42 +0100

    Small revert to previous DTO.

--- hooks/useProfile.ts
+++ hooks/useProfile.ts
@@ -8,10 +8,7 @@ export type ProfileDTO = {
   lastName: string;
 };
 
-export type UpdateProfileDTO = {
-  firstName: string;
-  lastName: string;
-};
+export type UpdateProfileDTO = Omit<ProfileDTO, "email">;
 
 export type ChangePasswordDTO = {
   oldPassword: string;
