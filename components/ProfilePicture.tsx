2645e94829142900c442b20aab204bb04940f994
Andreas Mertz <andreasmertz11@gmail.com>
2024-11-21 17:49:25 +0100

    Updates everywhere when updating profile picture

--- components/ProfilePicture.tsx
+++ components/ProfilePicture.tsx
@@ -1,8 +1,11 @@
-import { StyleProp, StyleSheet, Text, View, ViewStyle, Image } from "react-native";
-import { useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback, useState } from "react";
+import { StyleSheet, Text, View, Image, ViewStyle, StyleProp } from "react-native";
 import { ScaleSize, SharedStyles } from "../utils/SharedStyles";
 import { getContrastingTextColor, hashNameToColour } from "../utils/profileColors";
 import { BASE_URL } from "../utils/globals";
+import { useAuthentication } from "../providers/AuthenticationProvider";
+import initialsFromName from "../utils/initialFromName";
 
 type ProfilePictureProps = {
   label: string;
@@ -12,37 +15,48 @@ type ProfilePictureProps = {
   imageURI?: string | null;
 };
 
+/**
+ * Component to display a user's profile picture or their initials if the picture is not available.
+ *
+ * @param {Object} props - The properties object.
+ * @param {string} props.label - The label to display (usually the user's name).
+ * @param {string} props.userId - The ID of the user whose profile picture is to be displayed.
+ * @param {Object} [props.style] - Optional styles to apply to the container.
+ * @param {number} [props.fontSize] - Optional font size for the initials text.
+ * @param {string} [props.imageURI] - Optional URI for the user's profile picture, used to temporarily display a placeholder image.
+ *
+ * @returns {JSX.Element} The rendered profile picture component.
+ */
 export const ProfilePicture = ({ label, userId, style, fontSize, imageURI }: ProfilePictureProps) => {
   const [imageError, setImageError] = useState(false);
-  const colourFromName = hashNameToColour(label);
-  const colourTextContrast = getContrastingTextColor(colourFromName);
+  const [timestamp, setTimestamp] = useState(Date.now());
+  const { userId: loggedInUserId } = useAuthentication();
 
-  const displayName = label
-    .split(" ")
-    .map((part) => part[0])
-    .join("")
-    .toUpperCase();
+  /*
+   * React Navigation hook that re-executes logic whenever the screen gains focus.
+   * Used here to update the timestamp and force a profile picture refresh for the logged-in user.
+   */
+  useFocusEffect(
+    useCallback(() => {
+      if (userId === loggedInUserId) {
+        setTimestamp(Date.now());
+      }
+    }, [loggedInUserId, userId])
+  );
 
-  const uri = imageURI ?? `${BASE_URL}/images/users/${userId}.jpeg`;
+  const bgColor = hashNameToColour(label);
+  const textColor = getContrastingTextColor(bgColor);
+  const displayName = initialsFromName(label);
+  const uri = imageURI ?? `${BASE_URL}/images/users/${userId}.jpeg?${timestamp}`;
+  const handleImageError = () => !imageURI && setImageError(true);
 
   return (
-    <View style={[styles.ProfilePictureContainer, style, { backgroundColor: colourFromName }]}>
-      {uri && !imageError ? (
-        <Image
-          source={{ uri }}
-          style={styles.ProfileImage}
-          onError={() => {
-            if (!imageURI) {
-              setImageError(true);
-            }
-          }}
-        />
+    <View style={[styles.container, style, { backgroundColor: bgColor }]}>
+      {!imageError && uri ? (
+        <Image source={{ uri }} style={styles.image} onError={handleImageError} />
       ) : (
         <Text
-          style={[
-            styles.ProfilePictureText,
-            { color: colourTextContrast, fontSize: ScaleSize(fontSize ?? 50) },
-          ]}
+          style={[styles.text, { color: textColor, fontSize: ScaleSize(fontSize ?? 50) }]}
           adjustsFontSizeToFit={true}
           numberOfLines={1}>
           {displayName}
@@ -53,20 +67,19 @@ export const ProfilePicture = ({ label, userId, style, fontSize, imageURI }: Pro
 };
 
 const styles = StyleSheet.create({
-  ProfilePictureContainer: {
+  container: {
     ...SharedStyles.trueCenter,
     shadowRadius: 15,
     shadowOpacity: 0.2,
     borderRadius: 10000,
   },
-  ProfileImage: {
+  image: {
     width: "100%",
     height: "100%",
     borderRadius: 10000,
   },
-  ProfilePictureText: {
+  text: {
     textShadowColor: "black",
     textShadowRadius: 0.5,
-    fontSize: ScaleSize(50),
   },
 });
